88917fccbab596b8e53099784c075038a69ea9e2
branko98 <branko.j.98@gmail.com>
2019-06-25 18:59:14 +0200

    drugi_zadatak

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -1,7 +1,7 @@
 <template>
     <ul>
         <li v-for="(animal, index) in animals" :key="index">
-            {{ animal.vrsta }} - {{ animal.ime}} - {{ animal.datumRodjenja }}
+            {{ animal.vrsta }} - {{ animal.ime}} - {{ animal.datumRodjenja || 'Nepoznat'}}
         </li>
     </ul>
 </template>
@@ -13,7 +13,7 @@ export default {
             animals: [
                 { vrsta:'vrsta', ime:'Ime', datumRodjenja: '10.05.2009'},
                 { vrsta:'vrstaa', ime:'Imee', datumRodjenja: '18.05.2010'},
-                { vrsta:'vrstaaa', ime:'Imeee', datumRodjenja: '13.05.2011'},
+                { vrsta:'vrstaaa', ime:'Imeee'},
                 { vrsta:'vrstaaaa', ime:'Imeeee', datumRodjenja: '16.05.2001'},
                 { vrsta:'vrstaaaaa', ime:'Imeeeee', datumRodjenja: '15.05.2004'},
             ]
